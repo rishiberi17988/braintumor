1dbc58654a0fd0b1e4cec8e26d2b298809abfeb2
rishiberi17988 <155062010+rishiberi17988@users.noreply.github.com>
2024-06-25 15:04:48 +0530

    commit

--- app.py
+++ app.py
@@ -6,51 +6,47 @@ from PIL import Image
 from pathlib import Path
 
 
-# this is for saving images and prediction
+# Function for saving images and making predictions
 def save_image(uploaded_file):
     if uploaded_file is not None:
+        # Create the 'images' directory if it doesn't exist
+        os.makedirs("images", exist_ok=True)
+        
         save_path = os.path.join("images", "input.jpeg")
         with open(save_path, "wb") as f:
             f.write(uploaded_file.read())
         st.success(f"Image saved to {save_path}")
 
-        model = torch.load(Path('model/model.pt'))
+        # Load the model
+        model = torch.load(Path('artifacts/06_19_2024_22_47_31/model_training/model.pt'))
 
-
+        # Define the transformations
         trans = transforms.Compose([
             transforms.RandomHorizontalFlip(),
             transforms.Resize(224),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            ])
+        ])
 
+        # Open the image and apply the transformations
         image = Image.open(Path('images/input.jpeg'))
+        input_tensor = trans(image)
 
-        input = trans(image)
+        # Adjust the tensor shape if needed
+        input_tensor = input_tensor.view(1, 1, 224, 224).repeat(1, 3, 1, 1)
 
-        input = input.view(1, 1, 224, 224).repeat(1, 3, 1, 1)
-
-        output = model(input)
-
+        # Make the prediction
+        output = model(input_tensor)
         prediction = int(torch.max(output.data, 1)[1].numpy())
-        print(prediction)
 
-        if (prediction == 0):
-            print ('Normal')
+        # Display the prediction
+        if prediction == 0:
             st.text_area(label="Prediction:", value="Normal", height=100)
-        if (prediction == 1):
-            print ('PNEUMONIA')
+        elif prediction == 1:
             st.text_area(label="Prediction:", value="PNEUMONIA", height=100)
-
-
-
-
 
 
 if __name__ == "__main__":
     st.title("Xray lung classifier")
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
     save_image(uploaded_file)
-
-
-    
